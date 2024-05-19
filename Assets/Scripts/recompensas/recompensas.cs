0b5cecb5c5afe36811a29e8c7cadd6c96158eb51
DarioColombo <104810661+MonjeTibet28@users.noreply.github.com>
2024-05-19 17:13:00 -0500

    add
    funcionalidad de crear recompensas como su vista faltan detalles

--- Assets/Scripts/recompensas/recompensas.cs
+++ Assets/Scripts/recompensas/recompensas.cs
@@ -1,35 +1,40 @@
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class recompensas : MonoBehaviour
 {
-    public GameObject itemPrefab;  
-    public Transform container;    
+    public GameObject itemPrefab;
+    public Transform container;
 
     private List<GameObject> items = new List<GameObject>();
 
-    public TMP_InputField inputField, inputField1; 
+    public TMP_InputField inputField, inputField1;
 
     public void AsignarTextoDesdeCampo()
     {
-        string textoIngresado = inputField.text + inputField1.text;
-        AddItem(textoIngresado);
+        string textoIngresado = inputField.text;
+        string textoIngresado2 = inputField1.text;
+        AddItem(textoIngresado, textoIngresado2);
     }
 
-    public void AddItem(string value)
+    public void AddItem(string value, string value2)
     {
         GameObject newItem = Instantiate(itemPrefab, container);
 
-        TextMeshProUGUI textComponent = newItem.transform.Find("Text").GetComponent<TextMeshProUGUI>();
-        textComponent.text = value; 
+        TMP_InputField inputComponent = newItem.transform.Find("ProductNameInput").GetComponent<TMP_InputField>();
+        inputComponent.text = value;
 
-        Button deleteButton = newItem.transform.Find("DeleteButton").GetComponent<Button>();
-        Button editButton = newItem.transform.Find("EditButton").GetComponent<Button>();
+        TMP_InputField inputComponent2 = newItem.transform.Find("ProductPriceInput").GetComponent<TMP_InputField>();
+        inputComponent2.text = value2;
+
+        Button deleteButton = newItem.transform.Find("btnEliminar").GetComponent<Button>();
+        Button editButton = newItem.transform.Find("btnEditar").GetComponent<Button>();
 
         deleteButton.onClick.AddListener(() => DeleteItem(newItem));
-        editButton.onClick.AddListener(() => EditItem(textComponent));
+        editButton.onClick.AddListener(() => EditItem(inputComponent, inputComponent2));
 
         items.Add(newItem);
     }
@@ -40,9 +45,11 @@ public class recompensas : MonoBehaviour
         Destroy(item);
     }
 
-    private void EditItem(TextMeshProUGUI textComponent)
+    private void EditItem(TMP_InputField inputComponent, TMP_InputField inputComponent2)
     {
 
-        textComponent.text = "Nuevo Texto Editado";  
+
+        inputComponent.text = "Nuevo Texto Editado";
+        inputComponent2.text = "Nuevo Precio Editado";
     }
 }
